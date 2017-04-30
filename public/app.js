eb5310ae5bf62e5e28752d65e36b61c2e3fd6b64
Kristo Palo <kristopalo@hotmail.com>
2017-05-01 01:45:20 +0300

    first snow

---
+++ public/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var TyponautApp = angular.module('TyponautApp', ['ngMaterial']);
+
+TyponautApp.controller('TyponautController', function ($scope, $http) {
+    $scope.word = 'testword';
+
+    var words = ['test1', 'test2', 'test3', 'test4', 'test5'];
+
+    
+
+});
