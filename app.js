8ed54ddf438499b8638c2fd373516360731eb38f
Kristo Palo <kristopalo@hotmail.com>
2017-05-01 03:44:53 +0300

    non angular

--- app.js
+++ app.js
@@ -1,34 +1,9 @@
 'use strict';
 
-var TyponautApp = angular.module('TyponautApp', ['ngMaterial', 'btford.socket-io']);
+var TyponautApp = angular.module('TyponautApp', ['ngMaterial']);
 
-app.factory('socket', ['$rootScope', function ($rootScope) {
-    var socket = io.connect();
-
-    return {
-        on: function (eventName, callback) {
-            socket.on(eventName, callback);
-        },
-        emit: function (eventName, data) {
-            socket.emit(eventName, data);
-        }
-    };
-}]);
 
 TyponautApp.controller('TyponautController', function ($scope, $http, socket) {
     $scope.word = 'testword';
-
-    $scope.newCustomers = [];
-    $scope.currentCustomer = {};
-
-    $scope.join = function () {
-        socket.emit('add-customer', $scope.currentCustomer);
-    };
-
-    socket.on('notification', function (data) {
-        $scope.$apply(function () {
-            $scope.newCustomers.push(data.customer);
-        });
-    });
 
 });
--- server.js
+++ server.js
@@ -9,16 +9,33 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+var answered = false;
+var clients = [];
+
+io.sockets.on('connection', function(socket){
   console.log('a player connected');
+
+  clients.push(socket.id);
+  console.log('id ' + socket.id);
+
+  io.emit('new_word', "new word");
+
   socket.on('disconnect', function(){
     console.log('a player disconnected');
   });
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
   });
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('send', function(msg){
+    io.emit('new_word', "new new word");
+    socket.broadcast.to(socket.id).emit('new_word','You won!');
+    if (!answered) {
+        console.log('rerer');
+        console.log('idd:' + socket.id);
+        socket.broadcast.to(socket.id).emit('new_word','You won!');
+    } else {
+        socket.broadcast.to(socket.id).emit('new_word','You lost!');
+    }
   });
 });
 
